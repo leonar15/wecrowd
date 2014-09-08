15845c83b94d448b8ff41c279cc497ee606e1afa
Leo Hochberg <leo@wepay.com>
2014-09-08 15:26:46 -0700

    fixed spacing and added clearer comments for WePay config

--- application/config/wepay.php
+++ application/config/wepay.php
@@ -4,9 +4,9 @@ WePay::useStaging('65452','37e3a127fd', '2014-01-08');
 
 return array(
 
-        // Your Client ID
-	'client_id'       => '65452',
+    // Your WePay Client ID
+    'client_id'       => '65452',
 
-        // Your Client Secret
-	'client_secret'   => '37e3a127fd',
+    // Your WePay Client Secret
+    'client_secret'   => '37e3a127fd',
 );
